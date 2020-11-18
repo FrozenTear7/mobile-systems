6ece577cee8c0d32adb5890b8505147b293e1eb7
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-11-18 17:08:45 +0100

    Add working Lab3 with html info

--- Lab3/app/src/main/java/com/github/frozentear7/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/github/frozentear7/lab3/MainActivity.java
@@ -1,14 +1,172 @@
 package com.github.frozentear7.lab3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    public static String OPENWEATHER_WEATHER_QUERY = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&mode=html&appid=4526d487f12ef78b82b7a7d113faea64";
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private TextView latValueTextView;
+    private TextView lonValueTextView;
+    private WebView weatherWebView;
+
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        latValueTextView = findViewById(R.id.latValueTextView);
+        lonValueTextView = findViewById(R.id.lonValueTextView);
+        weatherWebView = findViewById(R.id.weatherWebView);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(final Location location) {
+                final double lat = (location.getLatitude());
+                final double lon = location.getLongitude();
+                new Thread(() -> updateWeather(lat, lon)).start();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+    }
+
+    private static class MyHandler extends Handler {
+        private final WeakReference<MainActivity> mActivity;
+
+        MyHandler(MainActivity activity) {
+            mActivity = new WeakReference(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            MainActivity activity = mActivity.get();
+            String lat = msg.getData().getString("lat");
+            String lon = msg.getData().getString("lon");
+            String web = msg.getData().getString("web");
+
+            activity.latValueTextView.setText(lat);
+            activity.lonValueTextView.setText(lon);
+            activity.weatherWebView.loadDataWithBaseURL("api.openweathermap.org", web, "text/html", "utf-8", null);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                accessLocation();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            // MY_PERMISSIONS_REQUEST_LOCATION is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        } else {
+            accessLocation();
+        }
+    }
+
+    private void accessLocation() {
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationProvider locationProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+        if (locationProvider != null) {
+            Toast.makeText(this, "Location listener registered!", Toast.LENGTH_SHORT).show();
+            try {
+                locationManager.requestLocationUpdates(locationProvider.getName(), 0, 0, this.locationListener);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "Location Provider is not avilable at the moment!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    Handler myHandler = new MyHandler(this);
+
+    private void updateWeather(double lat, double lon) {
+        String weather = getContentFromUrl(String.format(OPENWEATHER_WEATHER_QUERY, lat, lon));
+        Message m = myHandler.obtainMessage();
+        Bundle b = new Bundle();
+        b.putString("lat", String.valueOf(lat));
+        b.putString("lon", String.valueOf(lon));
+        b.putString("web", weather);
+        m.setData(b);
+        myHandler.sendMessage(m);
+    }
+
+    public String getContentFromUrl(String addr) {
+        String content = null;
+
+        Log.v("[GEO WEATHER ACTIVITY]", addr);
+        HttpURLConnection urlConnection = null;
+        URL url;
+        try {
+            url = new URL(addr);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            content = stringBuilder.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) urlConnection.disconnect();
+        }
+
+        return content;
     }
 }
